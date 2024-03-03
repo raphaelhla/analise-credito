a73537b26a5dec4cfb57b55ce5e3b7cac686c28c
raphaelhla <raphael.agra@ccc.ufcg.edu.br>
2024-03-03 01:49:56 -0300

    Adiciona Microsserviço de notificação

---
+++ notificacao/src/main/java/com/raphaelagra/notificacao/constante/MensagemConstante.java
@@ -0,0 +1,6 @@
+package com.raphaelagra.notificacao.constante;
+
+public class MensagemConstante {
+
+	public static final String PROPOSTA_EM_ANALISE = "Prezado(a) %s, sua proposta foi recebida por nossa equipe e será analisada. Em breve retornamos o contato";
+}
