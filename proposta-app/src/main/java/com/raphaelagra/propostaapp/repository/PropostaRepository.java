539be89bbe83b52bbbe18ce35ea4eeae2ccd1170
raphaelhla <raphael.agra@ccc.ufcg.edu.br>
2024-03-04 02:49:24 -0300

    build: add Dockerfile do proposta-app + configurações de build

--- proposta-app/src/main/java/com/raphaelagra/propostaapp/repository/PropostaRepository.java
+++ proposta-app/src/main/java/com/raphaelagra/propostaapp/repository/PropostaRepository.java
@@ -2,8 +2,11 @@ package com.raphaelagra.propostaapp.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.raphaelagra.propostaapp.entity.Proposta;
 
@@ -11,4 +14,9 @@ import com.raphaelagra.propostaapp.entity.Proposta;
 public interface PropostaRepository extends CrudRepository<Proposta, Long>{
 
 	List<Proposta> findAllByIntegradaIsFalse();
+
+	@Transactional
+	@Modifying
+	@Query(value = "UPDATE proposta SET aprovada = :aprovada, observacao = :observacao WHERE id = :id", nativeQuery = true)
+	void atualizarProposta(Long id, boolean aprovada, String observacao);
 }
